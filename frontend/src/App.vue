dd047d52db8f0eee0d3dcbe593a1c5d30e7db69f
Manuel17ms <manuel.manea@studenti.unitn.it>
2025-11-29 18:44:13 +0100

    Update App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="font-family: system-ui, 'Segoe UI', Roboto, 'Helvetica Neue', Arial; padding: 2rem; max-width:900px; margin:0 auto;">
+    <h1 style="text-align:center;">RF4 — Simulazione Consumo Energetico</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
