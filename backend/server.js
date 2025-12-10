84582aee6448a9f5c64d5742ad4aebb28331af86
Manuel17ms <manuel.manea@studenti.unitn.it>
2025-12-10 20:17:41 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./config/db');
-const simulationRoutes = require('./routes/simulationRoutes');
+const simulationRoutes = require('./routes/simulation.routes');
 require('dotenv').config();
 
 const app = express();
@@ -30,6 +30,7 @@ app.get('/', (req, res) => res.json({ message: 'RF4 Simulation Backend' }));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server listening on port ${PORT}`));
+
 
 
 
