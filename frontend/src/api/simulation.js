7e58320d932e1c09d261a437f6dc27e72bb2a217
Manuel17ms <manuel.manea@studenti.unitn.it>
2025-12-11 12:47:37 +0100

    Update simulation.js

--- frontend/src/api/simulation.js
+++ frontend/src/api/simulation.js
@@ -11,14 +11,15 @@ export function postSimulation(data) {
 }
 
 // Località
-export function getLocations() {
-  return API.get('/locations').then(res => res.data);
+export function getLocations() { 
+  return API.get('/locations'); 
 }
 
 // Confronto
 export function getCompare(locationId) {
   return API.get(`/simulation/compare/${locationId}`).then(res => res.data);
 }
+
 
 
 
