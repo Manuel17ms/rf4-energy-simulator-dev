3fa25ddd8090a5882fb5ced9d39235b8d45b7e93
Manuel17ms <manuel.manea@studenti.unitn.it>
2026-02-08 11:41:40 +0100

    Create HistoryChart.vue

---
+++ frontend/src/components/HistoryChart.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+} from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+)
+
+const props = defineProps({
+  history: Array
+})
+
+const chartData = {
+  labels: props.history.map(h => h.date),
+  datasets: [
+    {
+      label: 'kWh',
+      data: props.history.map(h => h.kwh),
+      borderColor: '#eaffd8'
+    }
+  ]
+}
+
+const chartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      labels: {
+        color: '#eaffd8'
+      }
+    }
+  },
+  scales: {
+    y: {
+      ticks: {
+        color: '#eaffd8'
+      }
+    },
+    x: {
+      ticks: {
+        color: '#eaffd8'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <Line :data="chartData" :options="chartOptions" />
+</template>
