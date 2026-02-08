de35bebd766d7a1b3873ef8eec3516897508d931
Manuel17ms <manuel.manea@studenti.unitn.it>
2026-02-08 11:40:32 +0100

    Create SimulationChart.vue

---
+++ frontend/src/components/SimulationChart.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+)
+
+const props = defineProps({
+  userValue: Number,
+  compareValue: Number
+})
+
+const chartData = {
+  labels: ['Your home', 'Neighborhood average'],
+  datasets: [
+    {
+      label: 'Energy Consumption (kWh)',
+      data: [props.userValue, props.compareValue],
+      backgroundColor: ['#eaffd8', '#b7e8a5']
+    }
+  ]
+}
+
+const chartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      labels: {
+        color: '#eaffd8'
+      }
+    }
+  },
+  scales: {
+    y: {
+      ticks: {
+        color: '#eaffd8'
+      }
+    },
+    x: {
+      ticks: {
+        color: '#eaffd8'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <Bar :data="chartData" :options="chartOptions" />
+</template>
