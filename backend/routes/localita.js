fa78175fc0495f10357c477989e879a245093d89
Manuel17ms <manuel.manea@studenti.unitn.it>
2026-02-11 22:38:01 +0100

    Update localita.js

--- backend/routes/localita.js
+++ backend/routes/localita.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const router = express.Router();
+import express from 'express'
 
-const localita = [
+const router = express.Router()
+
+export const localita = [
   { id: 'loc1', name: 'Piedicastello', climateFactor: 1.2 },
   { id: 'loc2', name: 'Madonna Bianca', climateFactor: 1.1 },
   { id: 'loc3', name: 'Solteri', climateFactor: 1.3 },
@@ -10,15 +11,10 @@ const localita = [
   { id: 'loc6', name: 'Cristo Re', climateFactor: 1.3 },
   { id: 'loc7', name: 'San Bartolomeo', climateFactor: 1.4 },
   { id: 'loc8', name: 'San Giuseppe', climateFactor: 1.0 }
-];
+]
 
-// API
-router.get(['/locations', '/localita', '/'], (req, res) => {
-  res.json(localita);
-});
+router.get('/locations', (req, res) => {
+  res.json(localita)
+})
 
-// 🔹 EXPORT DATI PER ALTRI ROUTER
-module.exports = {
-  router,
-  localita
-};
+export default router
