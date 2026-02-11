7c0608ba6ae6ff812a048a9ed37ee461618ee83f
Manuel17ms <manuel.manea@studenti.unitn.it>
2026-02-11 22:40:30 +0100

    Update compare.test.js

--- backend/tests/compare.test.js
+++ backend/tests/compare.test.js
@@ -1,33 +1,15 @@
 import request from 'supertest'
-import mongoose from 'mongoose'
 import app from '../server.js'
 
-describe('GET /api/simulation/compare', () => {
+describe('GET /compare', () => {
 
-  beforeAll(async () => {
-    await mongoose.connect(process.env.MONGODB_URI)
-  })
-
-  afterAll(async () => {
-    await mongoose.connection.close()
-  })
-
-  test('Confronto località valido', async () => {
+  test('Compare location', async () => {
 
     const res = await request(app)
       .get('/api/simulation/compare/loc1')
 
     expect(res.statusCode).toBe(200)
-    expect(res.body).toHaveProperty('estimatedConsumptionKWh')
-    expect(res.body).toHaveProperty('co2EquivalentKg')
-  })
 
-  test('Località inesistente', async () => {
-
-    const res = await request(app)
-      .get('/api/simulation/compare/loc999')
-
-    expect(res.statusCode).toBe(404)
   })
 
 })
