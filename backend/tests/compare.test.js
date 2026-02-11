ac63725254b09ea4322c1055d8147abeb5fc3ef7
Manuel17ms <manuel.manea@studenti.unitn.it>
2026-02-11 22:57:41 +0100

    Update compare.test.js

--- backend/tests/compare.test.js
+++ backend/tests/compare.test.js
@@ -1,5 +1,19 @@
+import mongoose from 'mongoose'
+import { connectDB } from '../config/db.js'
 import request from 'supertest'
 import app from '../server.js'
+
+beforeAll(async () => {
+
+  await connectDB(process.env.MONGODB_URI)
+
+})
+
+afterAll(async () => {
+
+  await mongoose.connection.close()
+
+})
 
 describe('GET /compare', () => {
 
